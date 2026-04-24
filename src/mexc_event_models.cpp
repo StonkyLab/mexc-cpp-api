a0c210824eb326ed74ed05b31c97f5d4db938d27
Vitezslav Kot <vitezslav.kot@stonky.cz>
2026-04-25 00:55:39 +0200

    Update copyright to Stonky s.r.o.

--- src/mexc_event_models.cpp
+++ src/mexc_event_models.cpp
@@ -3,7 +3,7 @@ MEXC Event Data Models
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 SPDX-License-Identifier: MIT
-Copyright (c) 2022 Vitezslav Kot <vitezslav.kot@gmail.com>.
+Copyright (c) 2022 Vitezslav Kot <vitezslav.kot@stonky.cz>, Stonky s.r.o.
 */
 
 #include "vk/mexc/mexc_event_models.h"
